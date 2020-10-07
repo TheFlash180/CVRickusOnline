de419aaaf62f36c3b97cb24b60c1b3cb4fd755aa
EC2 Default User <ec2-user@ip-172-31-19-14.ec2.internal>
2020-10-07 14:21:18 +0000

    Added extra pages

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,17 +1,15 @@
-console.log("sdjhsjdhjshdjshd");
-
-let https = new XMLHttpRequest();
+let site = new XMLHttpRequest();
 
 var city = "krugersdorp";
-https.open("GET", "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=4f15d36ca3825e612d82aa264ca86d9b");
-    https.send();
+site.open("GET", "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=4f15d36ca3825e612d82aa264ca86d9b");
+    site.send();
 
-    https.onload = () => {
+    site.onload = () => {
 
-    if(https.status === 200){
-      var vals = JSON.parse(https.response);
+    if(site.status === 200){
+      var vals = JSON.parse(site.response);
       console.log(vals);
     }else{
-      console.log(`error ${https.status} ${https.statusText}`);
+      console.log(`error ${site.status} ${site.statusText}`);
     }
 }
