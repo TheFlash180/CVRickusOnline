7445d1464d63e57de5b7d2f15d4a980e071d3835
EC2 Default User <ec2-user@ip-172-31-19-14.ec2.internal>
2020-10-08 09:15:25 +0000

    Fix permissions

--- tests/test.js
+++ tests/test.js
@@ -16,3 +16,53 @@ describe('Tests index', function() {
     });
   });
 });
+
+var about = require('../about.js');
+
+describe('Tests about', function() {
+  it('verifies successful response', function(done) {
+    index.get({ /* event */ }, { /* context */ }, (err, result) => {
+      try {
+        test.number(result.statusCode).is(200);
+        test.value(result).hasHeader('content-type', 'text/html');
+        done();
+      } catch(error) {
+        done(error);
+      }
+    });
+  });
+});
+
+'use strict';
+
+var contact = require('../contact.js');
+
+describe('Tests contact', function() {
+  it('verifies successful response', function(done) {
+    index.get({ /* event */ }, { /* context */ }, (err, result) => {
+      try {
+        test.number(result.statusCode).is(200);
+        test.value(result).hasHeader('content-type', 'text/html');
+        done();
+      } catch(error) {
+        done(error);
+      }
+    });
+  });
+});
+
+var projects = require('../projects.js');
+
+describe('Tests projects', function() {
+  it('verifies successful response', function(done) {
+    index.get({ /* event */ }, { /* context */ }, (err, result) => {
+      try {
+        test.number(result.statusCode).is(200);
+        test.value(result).hasHeader('content-type', 'text/html');
+        done();
+      } catch(error) {
+        done(error);
+      }
+    });
+  });
+});
